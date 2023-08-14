24a4bb50f36c27df2c6864e6c6ee86cec4bfcc4e
姜同远 <1474054587@qq.com>
2023-08-14 09:42:00 +0800

    style:entity.image gorm.column

--- model/entity/image.go
+++ model/entity/image.go
@@ -6,20 +6,19 @@ import (
 )
 
 type Image struct {
-	Id          int       `json:"id" gorm:"primary_key"`
-	Url         string    `json:"url"`
-	CreateTime  time.Time `json:"create_time"`
-	UpdateTime  time.Time `json:"update_time"`
-	Deleted     int       `json:"deleted"`
-	*ImageExtra `json:"extra"`
+	Id          int       `json:"id" gorm:"column:id;primary_key"`
+	Url         string    `json:"url" gorm:"column:url"`
+	CreateTime  time.Time `json:"create_time" gorm:"column:create_time;type:datetime(0);autoUpdateTime"`
+	UpdateTime  time.Time `json:"update_time" gorm:"column:update_time;type:datetime(0);autoUpdateTime"`
+	Deleted     int       `json:"deleted" gorm:"column:deleted"`
+	*ImageExtra `json:"extra" gorm:"column:extra"`
 }
 type ImageExtra struct{}
 
 func (Image) TableName() string {
 	return "image"
 }
+
 func CreateImage(image *Image) error {
-	image.CreateTime = time.Now()
-	image.UpdateTime = time.Now()
 	return global.Datasource.Create(&image).Error
 }
