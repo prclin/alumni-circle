add3cbc53e4252cfc867996513527b7a6f094902
姜同远 <1474054587@qq.com>
2023-08-14 15:27:48 +0800

    feat:添加课间模块

--- service/break.go
+++ service/break.go
@@ -1,7 +1,21 @@
 package service
 
-import "github.com/prclin/alumni-circle/model/entity"
+import (
+	"github.com/prclin/alumni-circle/model/entity"
+)
 
 func CreateBreak(aBreak *entity.Break) (err error) {
 	return entity.CreateBreak(aBreak)
+}
+
+func BreakExist(breakId int, accountId int) error {
+	aBreak := &entity.Break{
+		Id:        breakId,
+		AccountId: accountId,
+	}
+	return entity.GetBreak(aBreak)
+}
+
+func AddImageToBreak(binding *entity.ImageBreakBinding) error {
+	return entity.CreateOrUpdateImageBreakBinding(binding)
 }
