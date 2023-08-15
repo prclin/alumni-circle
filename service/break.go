d4e42faa261c62b80d91e2c3d5bb02c4babc9895
姜同远 <1474054587@qq.com>
2023-08-15 14:48:35 +0800

    feat:适量注释

--- service/break.go
+++ service/break.go
@@ -4,10 +4,12 @@ import (
 	"github.com/prclin/alumni-circle/model/entity"
 )
 
+// CreateBreak 创建课间
 func CreateBreak(aBreak *entity.Break) (err error) {
 	return entity.CreateBreak(aBreak)
 }
 
+// BreakExist 课间是否存在
 func BreakExist(breakId int, accountId int) bool {
 	aBreak := &entity.Break{
 		Id:        breakId,
