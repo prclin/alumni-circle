b3789183eea0e7824a71fac5b86567bd194f2fbd
imyttizy <1664619620@qq.com>
2023-08-24 16:02:27 +0800

    feat: finish put tag

--- controller/tag.go
+++ controller/tag.go
@@ -7,11 +7,13 @@ import (
 	"github.com/prclin/alumni-circle/model/po"
 	. "github.com/prclin/alumni-circle/model/response"
 	"github.com/prclin/alumni-circle/service"
+	"strconv"
 )
 
 func init() {
 	tag := core.ContextRouter.Group("/tag")
 	tag.POST("", PostTag)
+	tag.PUT("/:id", PutTag)
 }
 
 // PostTag 创建兴趣标签
@@ -29,6 +31,40 @@ func PostTag(c *gin.Context) {
 	//创建
 	tag, err := service.CreateTag(po.TTag{
 		Name: body.Name,
+	})
+	if err != nil {
+		Logger.Debug(err)
+		Server(c)
+		return
+	}
+	Ok(c, tag)
+}
+
+// PutTag 修改兴趣标签
+func PutTag(c *gin.Context) {
+	//获取参数
+	param := c.Param("id")
+	id, err := strconv.ParseUint(param, 10, 32)
+	if err != nil {
+		Logger.Debug(err)
+		Client(c)
+		return
+	}
+	var body struct {
+		Name  string  `json:"name" binding:"required"`
+		Extra *string `json:"extra" binding:"omitempty,json"`
+	}
+	err = c.ShouldBindJSON(&body)
+	if err != nil {
+		Logger.Debug(err)
+		Client(c)
+		return
+	}
+	//修改
+	tag, err := service.UpdateTag(po.TTag{
+		Id:    uint32(id),
+		Name:  body.Name,
+		Extra: body.Extra,
 	})
 	if err != nil {
 		Logger.Debug(err)
--- model/po/tag.go
+++ model/po/tag.go
@@ -5,7 +5,7 @@ import "time"
 type TTag struct {
 	Id         uint32    `json:"id"`
 	Name       string    `json:"name"`
-	Extra      string    `json:"extra"`
+	Extra      *string   `json:"extra"`
 	CreateTime time.Time `json:"create_time"`
 	UpdateTime time.Time `json:"update_time"`
 }
