7abac2e5d43754976126ffd75535f48d50fe6e84
imyttizy <1664619620@qq.com>
2023-08-24 18:55:12 +0800

    refactor: refactor tag

--- dao/tag.go
+++ dao/tag.go
@@ -15,9 +15,9 @@ func NewTagDao(tx *gorm.DB) *TagDao {
 
 func (td *TagDao) InsertBy(tag po.TTag) (uint32, error) {
 	var id uint32
-	sql := "insert into tag(name) value (?)"
+	sql := "insert into tag(name, state, extra) value (?,?,?)"
 	//插入
-	if err := td.Tx.Exec(sql, tag.Name).Error; err != nil {
+	if err := td.Tx.Exec(sql, tag.Name, tag.State, tag.Extra).Error; err != nil {
 		return 0, err
 	}
 	//查询主键
@@ -30,12 +30,12 @@ func (td *TagDao) InsertBy(tag po.TTag) (uint32, error) {
 
 func (td *TagDao) SelectById(id uint32) (po.TTag, error) {
 	var tag po.TTag
-	sql := "select id, name, extra, create_time, update_time from tag where id=?"
+	sql := "select id, name, state, extra, create_time, update_time from tag where id=?"
 	err := td.Tx.Raw(sql, id).First(&tag).Error
 	return tag, err
 }
 
 func (td *TagDao) UpdateTagBy(tag po.TTag) error {
-	sql := "update tag set name=?,extra=? where id=?"
-	return td.Tx.Exec(sql, tag.Name, tag.Extra, tag.Id).Error
+	sql := "update tag set name=?,state=?,extra=? where id=?"
+	return td.Tx.Exec(sql, tag.Name, tag.State, tag.Extra, tag.Id).Error
 }
