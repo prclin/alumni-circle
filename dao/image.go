ac9a2a22d237c5e631bd9301ebc2996e2a4c95b6
imyttizy <1664619620@qq.com>
2023-08-24 14:57:59 +0800

    feat: finish put photo wall

--- dao/image.go
+++ dao/image.go
@@ -2,7 +2,9 @@ package dao
 
 import (
 	"github.com/prclin/alumni-circle/model/entity"
+	"github.com/prclin/alumni-circle/model/po"
 	"gorm.io/gorm"
+	"strings"
 )
 
 type PhotoDao struct {
@@ -18,4 +20,23 @@ func (pd *PhotoDao) SelectPhotosByAccountId(accountId uint64) ([]entity.Photo, e
 	sql := "select i.url, pb.`order` from photo_binding as pb left join image as i on pb.image_id=i.id where pb.account_id=? order by pb.`order`"
 	err := pd.Tx.Raw(sql, accountId).Scan(&photos).Error
 	return photos, err
+}
+
+func (pd *PhotoDao) DeleteByAccountId(accountId uint64) error {
+	sql := "delete from photo_binding where account_id=?"
+	return pd.Tx.Exec(sql, accountId).Error
+}
+
+func (pd *PhotoDao) BatchInsertBy(bindings []po.TPhotoBinding) error {
+	if len(bindings) == 0 {
+		return nil
+	}
+	sql := "insert into photo_binding(account_id, image_id, `order`) values" //此处为goland报错
+	params := make([]interface{}, 0, len(bindings))
+	for _, binding := range bindings {
+		sql += "(?,?,?),"
+		params = append(params, binding.AccountId, binding.ImageId, binding.Order)
+	}
+	sql = strings.TrimSuffix(sql, ",")
+	return pd.Tx.Exec(sql, params...).Error
 }
--- service/account.go
+++ service/account.go
@@ -22,3 +22,26 @@ func GetPhotoWall(accountId uint64) ([]entity.Photo, error) {
 	pd := dao.NewPhotoDao(Datasource)
 	return pd.SelectPhotosByAccountId(accountId)
 }
+
+func UpdateAccountPhoto(accountId uint64, bindings []TPhotoBinding) error {
+	//开启事务
+	tx := Datasource.Begin()
+	defer tx.Commit()
+	pd := dao.NewPhotoDao(tx)
+	//删除原先的照片
+	err := pd.DeleteByAccountId(accountId)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	//插入新照片
+	for i := 0; i < len(bindings); i++ {
+		bindings[i].AccountId = accountId
+	}
+	err = pd.BatchInsertBy(bindings)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
