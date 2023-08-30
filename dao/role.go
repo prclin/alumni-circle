d84363af462baf6bad86da2855ca4c6f6372bee4
imyttizy <1664619620@qq.com>
2023-08-30 14:38:31 +0800

    feat: finish post role

--- dao/role.go
+++ dao/role.go
@@ -19,3 +19,24 @@ func (rd *RoleDao) SelectBindingByAccountId(accountId uint64) ([]TRoleBinding, e
 	err := rd.Tx.Raw(sql, accountId).Scan(&rb).Error
 	return rb, err
 }
+
+func (rd *RoleDao) InsertBy(role TRole) (uint32, error) {
+	var id uint32
+	sql := "insert into role(name, identifier, description, state) value (?,?,?,?)"
+	//插入
+	if err := rd.Tx.Exec(sql, role.Name, role.Identifier, role.Description, role.State).Error; err != nil {
+		return 0, err
+	}
+	//获取主键
+	if err := rd.Tx.Raw("select LAST_INSERT_ID()").First(&id).Error; err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func (rd *RoleDao) SelectById(id uint32) (TRole, error) {
+	var role TRole
+	sql := "select id, name, identifier, description, state, create_time,update_time from role where id=?"
+	err := rd.Tx.Raw(sql, id).First(&role).Error
+	return role, err
+}
--- model/role.go
+++ model/role.go
@@ -2,11 +2,22 @@ package model
 
 import "time"
 
+// TRole 角色表
+type TRole struct {
+	Id          uint32    `json:"id"`
+	Name        string    `json:"name"`
+	Identifier  string    `json:"identifier"`
+	Description string    `json:"description"`
+	State       uint8     `json:"state"`
+	CreateTime  time.Time `json:"create_time"`
+	UpdateTime  time.Time `json:"update_time"`
+}
+
 // TRoleBinding 角色绑定表
 type TRoleBinding struct {
 	AccountId  uint64
 	RoleId     uint32
-	Extra      string
+	Extra      *string
 	CreateTime time.Time
 	UpdateTime time.Time
 }
