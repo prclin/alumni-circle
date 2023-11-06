e13836c196f88d1a70d63c2c1170349358c97f44
imyttizy <1664619620@qq.com>
2023-11-06 23:02:31 +0800

    feat: add model Comment

---
+++ model/comment.go
@@ -0,0 +1,22 @@
+package model
+
+import "time"
+
+// TComment 评论表
+type TComment struct {
+	Id         uint64    `json:"id"`
+	ParentId   uint64    `json:"parent_id"`
+	BreakId    uint64    `json:"break_id"`
+	Content    string    `json:"content"`
+	LikeCount  uint32    `json:"like_count"`
+	Extra      *string   `json:"extra"`
+	CreateTime time.Time `json:"create_time"`
+	UpdateTime time.Time `json:"update_time"`
+}
+
+// Comment 评论实体
+type Comment struct {
+	TComment
+	Liked       bool         `json:"liked"`
+	AccountInfo *AccountInfo `json:"account_info"`
+}
