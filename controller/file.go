11249b3269e74abf0658665eaf00370c4b0c7d2f
imyttizy <1664619620@qq.com>
2023-11-10 15:39:02 +0800

    feat: finish post images

--- controller/file.go
+++ controller/file.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/prclin/alumni-circle/core"
+	_error "github.com/prclin/alumni-circle/error"
 	"github.com/prclin/alumni-circle/global"
 	"github.com/prclin/alumni-circle/model"
 	"github.com/prclin/alumni-circle/service"
@@ -18,14 +19,45 @@ func init() {
 	file.POST("/images", PostImages)
 }
 
+//PostImages 多图片上传，迭代中
 func PostImages(context *gin.Context) {
+	//解析token
+	claims, err := util.GetTokenClaims(context)
+	if err != nil {
+		util.Error(context, err)
+		return
+	}
 	// Multipart form
-	_, err := context.MultipartForm()
+	form, err := context.MultipartForm()
 	if err != nil {
 		model.Client(context)
 		return
 	}
 
+	//上传
+	files := form.File["files"]
+	images := make([]model.TImage, 0, len(files))
+	for _, file := range files {
+		//校验文件类型
+		cType := file.Header.Get("Content-Type")
+		if strings.Split(cType, "/")[0] != "image" {
+			global.Logger.Debug("文件类型错误!")
+			util.Error(context, _error.NewClientError("不支持的文件类型"))
+			return
+		}
+		//重命名文件
+		file.Filename = strconv.FormatInt(time.Now().UnixNano(), 10) /*当前时间戳*/ + "-" + strconv.FormatUint(claims.Id, 10) /*账户id*/ + "." + strings.Split(cType, "/")[1] /*扩展名*/
+		//上传文件到oss
+		image, err := service.UploadFileToOSS("alumni-circle", "development", file) //开发阶段，此处目录为development
+		if err != nil {
+			global.Logger.Debug(err)
+			util.Error(context, _error.InternalServerError)
+			return
+		}
+		images = append(images, image)
+	}
+
+	util.Ok(context, "上传成功", images)
 }
 
 // PostImage 上传图片
